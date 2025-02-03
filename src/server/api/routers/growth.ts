725ce52fc1784f7789bef3cb7d400c8fad4dcd09
Mainak <mainakdas104@gmail.com>
2025-02-03 23:54:07 +0530

    feat: add India states data and update growth and user profile routes

--- src/server/api/routers/growth.ts
+++ src/server/api/routers/growth.ts
@@ -1,43 +1,63 @@
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
-// import { z } from "zod";
+import { z } from "zod";
 
-const growthModelResponseDummy = {
-  predictions: {
-    height: Math.floor(Math.random() * 100) + 100,
-    weight: 47.7,
-  },
-  units: {
-    height: "cm",
-    weight: "kg",
-  },
-  validation: {
-    bmi: 24.4,
-    is_valid: true,
-  },
-};
+// const growthModelResponseDummy = {
+//   predictions: {
+//     height: Math.floor(Math.random() * 100) + 100,
+//     weight: 47.7,
+//   },
+//   units: {
+//     height: "cm",
+//     weight: "kg",
+//   },
+//   validation: {
+//     bmi: 24.4,
+//     is_valid: true,
+//   },
+// };
 
 export const growthRouter = createTRPCRouter({
-  addHeightImage: protectedProcedure
-    // .input(z.object({ image: z.instanceof(File) }))
-    .mutation(async ({ ctx }) => {
+  addHealthMetrics: protectedProcedure
+    .input(
+      z.object({
+        weight: z.number(),
+        height: z.number(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
       // Return the dummy response as if the model was called from the server with 2 seconds delay
       await new Promise((resolve) => setTimeout(resolve, 2000));
 
-      // save the image to the database
+      // save the metrics to the database
       return await ctx.db.user.update({
         where: {
           clerkId: ctx.userId,
         },
         data: {
+          weight: {
+            create: {
+              weight: input.weight,
+              addedById: ctx.userId,
+            },
+          },
           height: {
             create: {
-              height: growthModelResponseDummy.predictions.height,
+              height: input.height,
+              addedById: ctx.userId,
             },
           },
         },
-        include: {
-          height: true,
-        },
       });
+    }),
+
+  getHeightFromImage: protectedProcedure
+    .input(z.custom<File>())
+    .mutation(async () => {
+      // Return the dummy response as if the model was called from the server with 1.5 seconds delay
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+
+      const randomHeight = Math.floor(Math.random() * 100) + 100;
+
+      return randomHeight;
     }),
 });
--- src/server/api/routers/user-profile.ts
+++ src/server/api/routers/user-profile.ts
@@ -2,29 +2,35 @@ import { z } from "zod";
 
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
 import { currentUser } from "@clerk/nextjs/server";
+// import { TRPCError } from "@trpc/server";
 
 export const userProfileRouter = createTRPCRouter({
   createNewUser: protectedProcedure
     .input(
       z.object({
-        firstName: z.string(),
-        lastName: z.string().optional(),
         weight: z.number(),
         age: z.number(),
         gender: z.enum(["MALE", "FEMALE", "OTHER"]),
+        state: z.string(),
+        type: z.enum(["STUDENT", "EMPLOYEE", "PARENT"]),
       }),
     )
     .mutation(async ({ ctx, input }) => {
-      return await ctx.db.user.create({
+      const authUser = await currentUser();
+
+      const user = await ctx.db.user.create({
         data: {
           age: input.age,
           clerkId: ctx.userId,
-          firstName: input.firstName,
-          lastName: input.lastName,
-          weight: input.weight,
+          firstName: authUser!.firstName!,
+          lastName: authUser!.lastName!,
           gender: input.gender,
+          state: input.state,
+          type: input.type,
         },
       });
+
+      return user;
     }),
   getUserProfileData: protectedProcedure.query(async ({ ctx }) => {
     const [user, userData] = await Promise.all([
@@ -41,6 +47,12 @@ export const userProfileRouter = createTRPCRouter({
               createdAt: "desc",
             },
           },
+          weight: {
+            select: {
+              createdAt: true,
+              weight: true,
+            },
+          },
         },
       }),
     ]);
@@ -49,8 +61,20 @@ export const userProfileRouter = createTRPCRouter({
       ...userData,
       profilePicture: user?.imageUrl,
       latestHeight: userData?.height[0]?.height ?? 0,
+      latestWeight: userData?.weight[0]?.weight ?? 0,
     };
 
     return processedData;
+  }),
+  checkUserExists: protectedProcedure.query(async ({ ctx }) => {
+    const user = await ctx.db.user.findUnique({
+      where: { clerkId: ctx.userId },
+    });
+
+    if (!user) {
+      return false;
+    }
+
+    return true;
   }),
 });
