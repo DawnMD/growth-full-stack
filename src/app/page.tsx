e1690684be5c7a00961d1e220a7d7bc72dac3801
Mainak <mainakdas104@gmail.com>
2025-02-05 00:04:18 +0530

    feat: add sign in/out buttons to home page using Clerk authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,31 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useAuth, useClerk } from "@clerk/nextjs";
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Home page</div>;
+  const { isSignedIn } = useAuth();
+
+  const { signOut } = useClerk();
+
+  return (
+    <div>
+      {isSignedIn ? (
+        <Button
+          onClick={() =>
+            signOut({
+              redirectUrl: "/",
+            })
+          }
+        >
+          Sign out
+        </Button>
+      ) : (
+        <Button asChild>
+          <Link href="/onboard">Sign in</Link>
+        </Button>
+      )}
+    </div>
+  );
 }
